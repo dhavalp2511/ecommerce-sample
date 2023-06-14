97ea5fc7a102e13b500b6835055c0712c5a7fb36
Dhaval Patel <pateldhaval11r@gmail.com>
2023-06-14 07:20:14 +0530

    Fix dark mode

--- app/src/main/java/com/example/ecommercedemo/ui/MainApplication.kt
+++ app/src/main/java/com/example/ecommercedemo/ui/MainApplication.kt
@@ -1,8 +1,13 @@
 package com.example.ecommercedemo.ui
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class MainApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
 }
